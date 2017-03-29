f74c7ecbe04b6ac5f2d0b34322d7279b4bf245f2
Teun <teunnstaghouwer@gmail.com>
2017-03-29 15:13:12 +0200

    started with RabbitMQ

--- src/MQ/MyReceiver.java
+++ src/MQ/MyReceiver.java
@@ -1,38 +1,68 @@
 package MQ;
 
-import javax.jms.*;  
-import javax.naming.InitialContext;  
-  
-public class MyReceiver {  
-    public static void main(String[] args) {  
-        try{  
-            //1) Create and start connection  
-            InitialContext ctx=new InitialContext();  
-            QueueConnectionFactory f=(QueueConnectionFactory)ctx.lookup("myQueueConnectionFactory");  
-            QueueConnection con=f.createQueueConnection();  
-            con.start();  
-            //2) create Queue session  
-            QueueSession ses=con.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);  
-            //3) get the Queue object  
-            Queue t=(Queue)ctx.lookup("myQueue");  
-            //4)create QueueReceiver  
-            QueueReceiver receiver=ses.createReceiver(t);  
-              
-            //5) create listener object  
-            MyListener listener=new MyListener();  
-              
-            //6) register the listener object with receiver  
-            receiver.setMessageListener(listener);  
-              
-            System.out.println("Receiver1 is ready, waiting for messages...");  
-            System.out.println("press Ctrl+c to shutdown...");  
-            while(true){                  
-                Thread.sleep(1000);  
-            }  
-        }catch(Exception e){System.out.println(e);}  
-    }  
-  
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+public class MyReceiver {
+
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] argv) throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    Consumer consumer = new DefaultConsumer(channel) {
+      @Override
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+          throws IOException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+      }
+    };
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+  }
 }
+
+
+//import javax.jms.*;  
+//import javax.naming.InitialContext;  
+//  
+//public class MyReceiver {  
+//    public static void main(String[] args) {  
+//        try{  
+//            //1) Create and start connection  
+//            InitialContext ctx=new InitialContext();  
+//            QueueConnectionFactory f=(QueueConnectionFactory)ctx.lookup("myQueueConnectionFactory");  
+//            QueueConnection con=f.createQueueConnection();  
+//            con.start();  
+//            //2) create Queue session  
+//            QueueSession ses=con.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);  
+//            //3) get the Queue object  
+//            Queue t=(Queue)ctx.lookup("myQueue");  
+//            //4)create QueueReceiver  
+//            QueueReceiver receiver=ses.createReceiver(t);  
+//              
+//            //5) create listener object  
+//            MyListener listener=new MyListener();  
+//              
+//            //6) register the listener object with receiver  
+//            receiver.setMessageListener(listener);  
+//              
+//            System.out.println("Receiver1 is ready, waiting for messages...");  
+//            System.out.println("press Ctrl+c to shutdown...");  
+//            while(true){                  
+//                Thread.sleep(1000);  
+//            }  
+//        }catch(Exception e){System.out.println(e);}  
+//    }  
+//  
+//}
 
 //package MQ;
 //
--- src/MQ/MySender.java
+++ src/MQ/MySender.java
@@ -1,45 +1,68 @@
 package MQ;
 
-import java.io.BufferedReader;  
-import java.io.InputStreamReader;  
-import javax.naming.*;  
-import javax.jms.*;
-  
-public class MySender {  
-    public static void main(String[] args) {  
-        try  
-        {   //Create and start connection  
-            InitialContext ctx=new InitialContext();  
-            QueueConnectionFactory f=(QueueConnectionFactory)ctx.lookup("myQueueConnectionFactory");  
-            QueueConnection con=f.createQueueConnection();  
-            con.start();  
-            //2) create queue session  
-            QueueSession ses=con.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);  
-            //3) get the Queue object  
-            Queue t=(Queue)ctx.lookup("myQueue");  
-            //4)create QueueSender object         
-            QueueSender sender=ses.createSender(t);  
-            //5) create TextMessage object  
-            TextMessage msg=ses.createTextMessage();  
-              
-            //6) write message  
-            BufferedReader b=new BufferedReader(new InputStreamReader(System.in));  
-            while(true)  
-            {  
-                System.out.println("Enter Msg, end to terminate:");  
-                String s=b.readLine();  
-                if (s.equals("end"))  
-                    break;  
-                msg.setText(s);  
-                //7) send message  
-                sender.send(msg);  
-                System.out.println("Message successfully sent.");  
-            }  
-            //8) connection close  
-            con.close();  
-        }catch(Exception e){System.out.println(e);}  
-    }  
-}  
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class MySender {
+	private final static String QUEUE_NAME = "hello";
+	
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	    Connection connection = factory.newConnection();
+	    Channel channel = connection.createChannel();
+	    
+	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+	    String message = "Hello World!";
+	    channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+	    System.out.println(" [x] Sent '" + message + "'");
+	    
+	    channel.close();
+	    connection.close();
+	}
+}
+
+//import java.io.BufferedReader;  
+//import java.io.InputStreamReader;  
+//import javax.naming.*;  
+//import javax.jms.*;
+//  
+//public class MySender {  
+//    public static void main(String[] args) {  
+//        try  
+//        {   //Create and start connection  
+//            InitialContext ctx=new InitialContext();  
+//            QueueConnectionFactory f=(QueueConnectionFactory)ctx.lookup("myQueueConnectionFactory");  
+//            QueueConnection con=f.createQueueConnection();  
+//            con.start();  
+//            //2) create queue session  
+//            QueueSession ses=con.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);  
+//            //3) get the Queue object  
+//            Queue t=(Queue)ctx.lookup("myQueue");  
+//            //4)create QueueSender object         
+//            QueueSender sender=ses.createSender(t);  
+//            //5) create TextMessage object  
+//            TextMessage msg=ses.createTextMessage();  
+//              
+//            //6) write message  
+//            BufferedReader b=new BufferedReader(new InputStreamReader(System.in));  
+//            while(true)  
+//            {  
+//                System.out.println("Enter Msg, end to terminate:");  
+//                String s=b.readLine();  
+//                if (s.equals("end"))  
+//                    break;  
+//                msg.setText(s);  
+//                //7) send message  
+//                sender.send(msg);  
+//                System.out.println("Message successfully sent.");  
+//            }  
+//            //8) connection close  
+//            con.close();  
+//        }catch(Exception e){System.out.println(e);}  
+//    }  
+//}  
 
 //package MQ;
 //
