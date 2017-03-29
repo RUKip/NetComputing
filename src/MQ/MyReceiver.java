37fa83060c04ed56f7627099ccd0dc74ad3bfd1a
Teun <teunnstaghouwer@gmail.com>
2017-03-29 12:57:09 +0200

    Further trying

--- src/MQ/MyReceiver.java
+++ src/MQ/MyReceiver.java
@@ -33,3 +33,41 @@ public class MyReceiver {
     }  
   
 }
+
+//package MQ;
+//
+//import javax.annotation.Resource;
+//import javax.ejb.*;
+//import javax.jms.*;
+//import java.util.Enumeration;
+// 
+////@Stateless
+//public class MyReceiver {
+// 
+//    @Resource(mappedName = "jms/JMSConnectionFactory")
+//    private ConnectionFactory connectionFactory;
+// 
+//    @Resource(mappedName = "jms/myQueue")
+//    Queue myQueue;
+// 
+//    private String message;
+// 
+//    public String receiveMessage() {
+//        try {
+//            Connection connection = connectionFactory.createConnection();
+//            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//            QueueBrowser queueBrowser = session.createBrowser(myQueue);
+//            Enumeration enumeration = queueBrowser.getEnumeration();
+//            while (enumeration.hasMoreElements()) {
+//                TextMessage o = (TextMessage) enumeration.nextElement();
+//               return "Receive " + o.getText();
+// 
+//            }
+//            session.close();
+//            connection.close();
+//        } catch (JMSException e) {
+//            e.printStackTrace();
+//        }
+//        return "";
+//    }
+//}
--- src/MQ/MySender.java
+++ src/MQ/MySender.java
@@ -40,3 +40,40 @@ public class MySender {
         }catch(Exception e){System.out.println(e);}  
     }  
 }  
+
+//package MQ;
+//
+//import javax.annotation.Resource;
+//import javax.jms.*;
+// 
+////@Stateless
+//public class MySender {
+// 
+//    @Resource(mappedName = "jms/JMSConnectionFactory")
+//    private ConnectionFactory connectionFactory;
+// 
+//    @Resource(mappedName = "jms/myQueue")
+//    Queue queue;
+// 
+//    public void sendMessage(String message) {
+//        MessageProducer messageProducer;
+//        TextMessage textMessage;
+//        try {
+//            Connection connection = connectionFactory.createConnection();
+//            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//            messageProducer = session.createProducer(queue);
+//            textMessage = session.createTextMessage();
+//            //textMessage.setJMSDeliveryMode(DeliveryMode.NON_PERSISTENT);
+// 
+//            textMessage.setText(message);
+//            messageProducer.send(textMessage);
+// 
+//            messageProducer.close();
+//            session.close();
+//            connection.close();
+//        } catch (JMSException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}  
